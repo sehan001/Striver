36954a554d005ff19d1c6ed39a5213f888acb552
Mohammed Sehan <nmsehan.001@gmail.com>
2023-09-14 01:02:14 +0530

    Create GCD(optimal).cpp

---
+++ GCD(optimal).cpp
@@ -0,0 +1,29 @@
+/*cpp code to find the highest common factor using euclid division algorithm*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    while(a>0 & b>0)
+    {
+        if(a>b)
+        {
+            a=a%b;
+        }
+        else
+        {
+            b=b%a;
+        }
+    }
+    if(a==0)
+    {
+        cout<<b;
+    }
+    else
+    {
+        cout<<a;
+    }
+    
+    return 0;
+}
